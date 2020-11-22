e23bbc800767f21a27fca015d0b6ff4368fde980
krayddy <50516437+krayddy@users.noreply.github.com>
2020-11-22 16:41:15 +0500

    added help_handler

--- main.py
+++ main.py
@@ -6,7 +6,12 @@ bot = telebot.TeleBot("1284427462:AAHPyWAvdRGT104ol7fCp4PXELJmAi55EwU")
 
 @bot.message_handler(commands=['start'])
 def start_handler(message):
-    bot.send_message(message.from_user.id, "Привет! Это бот-помощник по дисциплине Индивидуальный проект.")
+    bot.send_message(message.from_user.id, "Привет! Это бот-помощник по дисциплине Индивидуальный проект. Комманда /help поможет начать")
+
+
+@bot.message_handler(commands=['help'])
+def help_handler(message):
+    bot.send_message(message.from_user.id, "Доступные команды: \n /file \n /start")
 
 
 @bot.message_handler(commands=['file'])
